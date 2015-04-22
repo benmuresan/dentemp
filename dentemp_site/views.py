4d4ad1ac696e68098c3051865b8870b0f05c57ce
Ben Muresan <benmuresan@yahoo.com>
2015-04-21 18:21:48 -0700

    Added Calendar

--- dentemp_site/views.py
+++ dentemp_site/views.py
@@ -31,68 +31,11 @@ def contact(request):
                   "contact.html")
 
 
-# def log_in(request):
-#     # If the request is a HTTP POST, try to pull out the relevant information.
-#     if request.method == 'POST':
-#         # Gather the username and password provided by the user.
-#         # This information is obtained from the login form.
-#                 # We use request.POST.get('<variable>') as opposed to request.POST['<variable>'],
-#                 # because the request.POST.get('<variable>') returns None, if the value does not exist,
-#                 # while the request.POST['<variable>'] will raise key error exception
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#
-#         # Use Django's machinery to attempt to see if the username/password
-#         # combination is valid - a User object is returned if it is.
-#         user = authenticate(username=username, password=password)
-#         print user
-#         # If we have a User object, the details are correct.
-#         # If None (Python's way of representing the absence of a value), no user
-#         # with matching credentials was found.
-#         if user:
-#
-#             # Is the account active? It could have been disabled.
-#             print "if user hit"
-#             if user.is_active:
-#                 print("user is active")
-#                 # If the account is valid and active, we can log the user in.
-#                 # We'll send the user back to the homepage.
-#                 login(request, user)
-#                 return render(request, '/user_dash/')
-#             else:
-#                 # An inactive account was used - no logging in!
-#                 return render(request, 'account_disabled.html')
-#         else:
-#             return render(request, 'incorrect_login.html')
-#
-#
-#     # The request is not a HTTP POST, so display the login form.
-#     # This scenario would most likely be a HTTP GET.
-#     else:
-#         # No context variables to pass to the template system, hence the
-#         # blank dictionary object...
-#         return render(request, 'log_in.html', {})
-#     # if request.POST:
-#     #     username = request.POST["username"]
-#     #     password = request.POST["password"]
-#     #     user = User.objects.create_user(username, password)
-#     #     user.save()
-#     #     profile = UserProfile()
-#     #     profile.user = user
-#     #     profile.employee_type = request.POST["type"]
-#     #     profile.save()
-#     #     return redirect("user_dash")
-#     #
-#     # return render(request,
-#     #               "log_in.html")
-
-
 def log_in(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
-        print user
         if user is not None:
             print "if user hit"
             if user.is_active:
